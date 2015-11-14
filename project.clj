dc15157d1367fb79e18d25c218521dba086d961d
unknown <gzbichorski@gmail.com>
2015-11-14 16:37:10 -0600

    project.clj

--- project.clj
+++ project.clj
@@ -1,13 +1,26 @@
 (defproject chatter "0.1.0-SNAPSHOT"
-  :description "GRETCHENISAWESOME: write description"
+  :description "clojure web app for displaying messages"
   :url "http://example.com/GRETCHENISAWESOME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
                  [ring/ring-defaults "0.1.5"]
-                 [hiccup "1.0.5"]]
-  :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler chatter.handler/app}
+                 [ring/ring-jetty-adapter "1.3.2"]
+                 [hiccup "1.0.5"]
+                 [hickory "0.5.4"]
+                 [environ "1.0.0"]]
+  :plugins [[lein-ring "0.9.7"]
+ [lein-environ "1.0.0"]]
+  :ring {:handler chatter.handler/app
+         :init chatter.handler/init
+         :destroy chatter.handler/destroy}
+  :aot :all
+  :main chatter.handler
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]]}
+:production
+   {:ring
+    {:open-browser? false, :stacktraces? false, :auto-reload? false}
+    :env {production true}}}
+  :uberjar-name "chatter-standalone.jar")
