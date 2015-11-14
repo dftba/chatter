1a5e52971ec7c291636b526623f7a6531c909182
unknown <gzbichorski@gmail.com>
2015-11-14 13:06:42 -0600

    making changes

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chatter "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "GRETCHENISAWESOME: write description"
+  :url "http://example.com/GRETCHENISAWESOME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
